2c645487fdafb8e153015fbb218868b298a0129d
Bhumit Pachani <bhumitpachani@gmail.com>
2026-05-12 19:19:11 +0530

    Refactor API handler to use edge runtime and streamline fetch logic

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,7 @@
 import serverEntry from '../src/server';
 
-export const config = {
-  runtime: 'edge',
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return await serverEntry.fetch(request as Request, undefined, undefined);
+  },
 };
-
-export default async function handler(request: Request): Promise<Response> {
-  return await serverEntry.fetch(request as Request);
-}
